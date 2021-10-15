968c915e9c2de2f1dac115d6a0018069479dcc36
Brandyn Britton <Brandynbb96@gmail.com>
2021-10-15 08:28:18 +0800

    update modal settings

--- src/app.js
+++ src/app.js
@@ -51,16 +51,14 @@ const getRandomBudget = async (budget) => {
 document.addEventListener("DOMContentLoaded", async function (event) {
 
     MicroModal.init({
-        onShow: modal => console.info(`${modal.id} is shown`), // [1]
-        onClose: modal => console.info(`${modal.id} is hidden`), // [2]
         openTrigger: 'data-custom-open', // [3]
         closeTrigger: 'data-custom-close', // [4]
         openClass: 'is-open', // [5]
         disableScroll: true, // [6]
         disableFocus: false, // [7]
-        awaitOpenAnimation: false, // [8]
-        awaitCloseAnimation: false, // [9]
-        debugMode: true // [10]
+        awaitOpenAnimation: true, // [8]
+        awaitCloseAnimation: true, // [9]
+        debugMode: false // [10]
     });
 
 
